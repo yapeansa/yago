beb5579368a2a4bd86f876f24bb6ef8b93571a54
yapeansa <yago.pereira@estudante.ufjf.br>
2024-02-12 21:50:21 -0300

    Mais projetos.

---
+++ src/pages/Projetos/ContainerProjetos/index.jsx
@@ -0,0 +1,77 @@
+import { styled } from "styled-components";
+
+const ContainerProjetos = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 1.5rem;
+    width: auto;
+    padding: 0 1rem;
+    .flip-card {
+        flex-basis: 30%;
+        perspective: 1000px;
+        width: 100%;
+        height: 300px;
+        background-color: transparent;
+        &:hover {
+            .flip-card-inner {
+                transform: rotateY(180deg);
+            }
+        }
+        .flip-card-inner {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            transition: transform 0.8s;
+            transform-style: preserve-3d;
+            .flip-card-front {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                -webkit-backface-visibility: hidden;
+                backface-visibility: hidden;
+                background-color: #bbb;
+                color: black;
+                img {
+                    border: 4px solid #3e3574;
+                    width: 100%;
+                    height: 300px;
+                    object-fit: fit;
+                }
+            }
+            .flip-card-back {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                -webkit-backface-visibility: hidden;
+                backface-visibility: hidden;
+                background-color: #3e3574;
+                color: white;
+                transform: rotateY(180deg);
+                display: flex;
+                align-items: center;
+                justify-content: flex-start;
+                text-align: left;
+                padding: 0 1.5rem;
+                .info-card {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 1.5rem;
+                    ul {
+                        list-style: none;
+                        display: flex;
+                        gap: 1rem;
+                        a {
+                            display: block;
+                            color: var(--branco);
+                            border: 1px solid var(--branco);
+                            padding: 6px 15px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export default ContainerProjetos;
--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -2,6 +2,7 @@ import { styled } from "styled-components";
 import Container from "../../components/Container";
 import Secao from "../../components/ContainerSecao";
 import Titulo from "../../components/Titulo";
+import ContainerProjetos from "./ContainerProjetos";
 import projetos from "./projetos.json";
 
 const Paragrafo = styled.p`
@@ -9,63 +10,6 @@ const Paragrafo = styled.p`
     font-size: 1.5rem;
     margin: 3.5rem 0;
     line-height: 1.5;
-`;
-
-const ContainerProjetos = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 1rem;
-    .flip-card {
-        flex-basis: 30%;
-        perspective: 1000px;
-        width: 100%;
-        height: 300px;
-        background-color: transparent;
-        &:hover {
-            .flip-card-inner {
-                transform: rotateY(180deg);
-            }
-        }
-        .flip-card-inner {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            text-align: center;
-            transition: transform 0.8s;
-            transform-style: preserve-3d;
-            .flip-card-front {
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                -webkit-backface-visibility: hidden;
-                backface-visibility: hidden;
-                background-color: #bbb;
-                color: black;
-                img {
-                    border: 4px solid #3e3574;
-                    width: 100%;
-                    height: 300px;
-                    object-fit: fit;
-                }
-            }
-            .flip-card-back {
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                -webkit-backface-visibility: hidden;
-                backface-visibility: hidden;
-                background-color: #3e3574;
-                color: white;
-                transform: rotateY(180deg);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                div { display: flex; flex-direction: column; gap: 1rem; }
-            }
-        }
-    }
-    .rotacionar {}
 `;
 
 const Projetos = () => {
@@ -84,10 +28,17 @@ const Projetos = () => {
                                         <img src={projeto.imagem} alt="Avatar" />
                                     </div>
                                     <div className="flip-card-back">
-                                        <div>
+                                        <div className="info-card">
                                             <h1>{projeto.nome}</h1>
                                             <p>{projeto.descricao}</p>
-                                            <p>{projeto.caminho}</p>
+                                            <ul>
+                                                <li>
+                                                    <a href={projeto.repositorio} target="_blank">Repositório</a>
+                                                </li>
+                                                <li>
+                                                    <a href={projeto.caminho} target="_blank">Visitar</a>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
                                 </div>
