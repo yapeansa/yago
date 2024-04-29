e52d5b08dcfe94366ff158abc2388f73d4bdef53
yapeansa <yago.pereira@estudante.ufjf.br>
2024-04-29 19:46:04 -0300

    Alterando título e imagem de fundo.

---
+++ src/components/Titulo/TituloEstilizado/index.jsx
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+
+const TituloEstilizado = styled.h2`
+    font-family: var(--fonte-secundaria);
+    font-weight: 700;
+    font-size: 2rem;
+    text-transform: uppercase;
+    position: relative;
+    display: inline-block;
+    padding: 15px 0;
+    color: ${props => props.$cordotexto ? props.$cordotexto : '#000'};
+    &::before {
+        content: '<';
+        position: absolute;
+        left: -2.5rem;
+        top: -.3rem;
+        color: var(--primeira-cor);
+        font-family: var(--fonte-primaria);
+        font-weight: normal;
+        font-size: 4rem;
+    }
+    &::after {
+        content: '/>';
+        position: absolute;
+        right: -4rem;
+        top: -.3rem;
+        color: var(--primeira-cor);
+        font-family: var(--fonte-primaria);
+        font-weight: normal;
+        font-size: 4rem;
+    }
+`;
+
+export default TituloEstilizado;
--- src/components/Titulo/index.jsx
+++ src/components/Titulo/index.jsx
@@ -1,25 +1,4 @@
-import styled from "styled-components";
-
-const TituloEstilizado = styled.h2`
-    font-family: var(--fonte-secundaria);
-    font-weight: 700;
-    font-size: 2rem;
-    text-transform: uppercase;
-    position: relative;
-    display: block;
-    padding: 15px 0;
-    color: ${props => props.$cordotexto ? props.$cordotexto : '#000'};
-    &::before {
-        content: '';
-        position: absolute;
-        left: 50%;
-        margin-left: -42px;
-        bottom: 0;
-        width: 84px;
-        height: 3px;
-        background-color: ${props => props.$cordaborda};
-    }
-`;
+import TituloEstilizado from "./TituloEstilizado";
 
 const Titulo = ({ corDaBorda, corDoTexto, children }) => {
     return (
