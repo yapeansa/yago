8b8891fd87510cfd805c6b74f1b9379a2c9922db
yapeansa <yago.pereira@estudante.ufjf.br>
2024-05-13 08:48:44 -0300

    Implementando EmailJS para envio de mensagens via formulário de contato.

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import Secao from "@/components/ContainerSecao";
 import Titulo from "@/components/Titulo";
 import ContainerContato from "./ContainerContato";
@@ -27,6 +29,24 @@ const Contato = () => {
         }
     };
 
+    // Here, we're using React EmailJS to send e-mail from contact form.
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_oumd0lp', 'template_e8zek0j', form.current, {
+            publicKey: 'o5T1d5IHXaJyiTE7D',
+        })
+            .then(() => {
+                console.log('SUCCESS!');
+                e.target.reset();
+            }, (error) => {
+                console.log('FAILED...', error.text);
+            },);
+    };
+
     return (
         <div id="contato">
             <Secao corDeFundo="var(--darker)">
@@ -38,11 +58,11 @@ const Contato = () => {
                             <SiGmail size={30} color="var(--cor-primaria)" /> yapeansa@gmail.com
                         </a>
                         <p>Ou preencha o formulário abaixo com seus dados e sua mensagem.</p>
-                        <form method="post" action="https://formspree.io/f/xnqlkqnk">
+                        <form ref={form} onSubmit={sendEmail}>
                             <div className="engloba_texto_email">
                                 <input
                                     type="text"
-                                    name="nome"
+                                    name="user_name"
                                     minLength={3}
                                     onFocus={() => aoInteragir('nome')}
                                     onBlur={() => aoInteragir('nome')}
@@ -51,7 +71,7 @@ const Contato = () => {
                                 />
                                 <input
                                     type="email"
-                                    name="email"
+                                    name="user_email"
                                     minLength={11}
                                     onFocus={() => aoInteragir('email')}
                                     onBlur={() => aoInteragir('email')}
@@ -61,7 +81,7 @@ const Contato = () => {
                             </div>
                             <label>
                                 <textarea
-                                    name="mensagem"
+                                    name="message"
                                     rows="5"
                                     minLength={200}
                                     onFocus={() => aoInteragir()}
